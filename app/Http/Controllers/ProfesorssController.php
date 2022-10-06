00cc72b0e530c982b3bb144b8f57514dcd8157ea
Nahuel Wagner <nahuel.wagner@adsmovil.com>
2022-10-06 16:25:12 -0300

    First Commit

---
+++ app/Http/Controllers/ProfesorssController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Profesorss;
+use Illuminate\Http\Request;
+
+class ProfesorssController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Profesorss  $profesorss
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profesorss $profesorss)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Profesorss  $profesorss
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profesorss $profesorss)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Profesorss  $profesorss
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profesorss $profesorss)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Profesorss  $profesorss
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profesorss $profesorss)
+    {
+        //
+    }
+}
---
+++ database/factories/ProfesorssFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Profesorss;
+use Faker\Generator as Faker;
+
+$factory->define(Profesorss::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
